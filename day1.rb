ecbe9629612ea43c605f8d2b35155124fb44eb1e
Gurucharan <gurucharan@saastrail.com>
2025-08-11 16:13:44 +0530

    after add improved code

--- day1.rb
+++ day1.rb
@@ -10,8 +10,39 @@ numbers.map do |n|
 	end
 end
 
+#improveed code
+
+(1..100).each do |n|
+  if n % 15 == 0
+    puts "FizzBuzz"
+  elsif n % 3 == 0
+    puts "Fizz"
+  elsif n % 5 == 0
+    puts "Buzz"
+  else
+    puts n
+  end
+end
+
+#Why % 15? Because 15 is the least common multiple (LCM) of 3 and 5, so it checks for both at once.
+
+
+#⚠️ Possible improvement:
+#If someone passes a sentence like "Madam, I'm Adam", it will fail because of spaces and punctuation.
+
+# You might want to remove non-alphanumeric characters.
+
+
 
 def palindrome?(word)
   word.downcase == word.downcase.reverse
 end
-
+#improved code below
+s
+def palindrome?(text)
+  cleaned = text.downcase.gsub(/[^a-z0-9]/, "")
+  cleaned == cleaned.reverse
+end
+# palindrome?("Madam")            # true
+# palindrome?("Hello")            # false
+# palindrome?("Madam, I'm Adam")  # true
